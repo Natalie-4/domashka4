01a32237e8a71d6c57327a1fc859e9347ca24ed9
Natalie <itlaym@yandex.ru>
2022-09-27 13:33:25 +0700

    solve task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.WriteLine("Введите первое число: ");
+int A =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B =  Convert.ToInt32(Console.ReadLine());
+
+int Power(int x, int y)
+{
+    int n = 1;
+    for(int j = 1; j <= y; j++)
+{
+    n = n*x;
+}
+return n;
+}
+
+int N = Power(A,B);
+Console.WriteLine(N);
+
